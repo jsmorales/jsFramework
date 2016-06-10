e2f60077c19018196c020e454b011d88c1288941
Johan Morales <jhellmetal2000@gmail.com>
2016-06-10 12:44:39 -0500

    Se agrega instalador del framework, solo funciona para ubuntu/debian, se debe tener git instalado, por el momento solo funciona la descarga del framework y la restauración de la bd(por ahora de prueba), falta que se cree un archivo de datos.php con las credenciales de conexion a la bd para que quede listo para usar.

--- jsFramework_InstallDebian.sh
+++ jsFramework_InstallDebian.sh
@@ -5,59 +5,11 @@
 #-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
-
-#Variable de la ruta del apache de debian
-RUTA_APACHE="/var/www/html/prueba_install"
-
-#Verificar que git este instalado
-#http://blog.desdelinux.net/como-saber-si-un-paquete-se-encuentra-instalado-o-no-de-manera-facil-y-rapida/
-#Guardar el resultado de un comando en una variable
-#http://es.ccm.net/faq/2130-como-guardar-el-resultado-de-un-comando-en-una-variable
-INSTALL_GIT=$(dpkg -l | grep git)
-
-#saber si una variable es null
-#http://serverfault.com/questions/7503/how-to-determine-if-a-bash-variable-is-empty
-
-if [ "$INSTALL_GIT" = "" ] || [ -z "$INSTALL_GIT" ] ; then
-	#statements
-	echo "Git---ERROR [Instale git e intente correr la instalación de nuevo.]"
-else
-	echo "Git---OK [Continuando la instalación.]"
-
-	#ir a la ruta de apache y clonar el proyecto desde git
-	cd $RUTA_APACHE
-	#lista las carpetas del directorio
-	ls -l
-	#verificar que no exista una carpeta llamada jsFramework
-	if [ -d "$RUTA_APACHE/jsFramework" ];
-	then
-		echo "DIR /jsFramework---ERROR [Ya existe un directorio con este nombre.]"
-	else
-		echo "DIR /jsFramework---OK [Continuando la instalación.]"
-		echo "Clonando git -> [DIR /jsFramework]"
-		#------------------------------------------------------------------------------------------------
-		#clonar en esta ruta el proyecto desde git
-		git clone https://github.com/jsmorales/jsFramework.git && chmod -R 777 "$RUTA_APACHE/jsFramework"
-		#------------------------------------------------------------------------------------------------
-		#verificar de nuevo que el directorio jsFramework existe para
-		#saber si se clonó correctamente
-		if [ -d "$RUTA_APACHE/jsFramework" ]; then
-			#statements
-			echo "git clone /jsFramework---OK [Se clonó el framework correctamente.]"
-
-			#lista las carpetas del directorio
-			ls -l | grep jsFramework
-
-			#------------------------------------------------------------------------
-			#reestablecer la BD del framework
-			#------------------------------------------------------------------------
-
-		else
-			echo "git clone /jsFramework---ERROR [Hubo un error al clonar el framework.]"
-		fi
-		#-------------------------------------------------------------
-	fi
-fi
-
+#aca debe ir el nombre del archivo que se deje por defecto
+echo "creando BD"
+mysqladmin -u root -p create unaMas
+#VERIFICAR
+SALIDA=$(mysqlcheck --check unaMas --user="root" --password="Lunel2016")
+echo $SALIDA
 #echo $INSTALL_GIT
 #-----------------------------------------------------------------------------
