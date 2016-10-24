439aef845cd0671e300c6b92169c07721c67b9a3
jsmorales <jhellmetal2000@gmail.com>
2016-10-24 09:31:57 -0500

    Se modifica la instalacion del framework para que se pueda utilizar desde cualquier parte dentro de la carpeta /usr/bin/ y se le quito la extension .sh para que solo sea la palabra jsFramework y los parametros.

---
+++ DAO/paginaDAO.php
@@ -0,0 +1,14 @@
+<?php
+	/**/
+	include_once 'genericoDAO.php';
+		
+	class paginaDAO extends GenericoDAO {
+		
+		public $q_general;
+		
+		
+		//Funciones------------------------------------------
+		//Espacio para las funciones en general de esta clase.
+		
+	}
+?>
---
+++ DAO/pagina_1DAO.php
@@ -0,0 +1,14 @@
+<?php
+	/**/
+	include_once 'genericoDAO.php';
+		
+	class pagina_1DAO extends GenericoDAO {
+		
+		public $q_general;
+		
+		
+		//Funciones------------------------------------------
+		//Espacio para las funciones en general de esta clase.
+		
+	}
+?>
